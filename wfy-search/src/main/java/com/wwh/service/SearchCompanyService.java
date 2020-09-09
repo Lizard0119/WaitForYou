74fed50294b180cf9261c78705e5dd01c27d593b
Administrator <543343254@qq.com>
2020-09-09 20:00:28 +0800

    武文豪9/9晚自习更新

--- wfy-search/src/main/java/com/wwh/service/SearchCompanyService.java
+++ wfy-search/src/main/java/com/wwh/service/SearchCompanyService.java
@@ -2,6 +2,8 @@ package com.wwh.service;
 
 import com.wwh.pojo.BaseResp;
 
+import java.io.IOException;
+
 public interface SearchCompanyService {
-    BaseResp search(String index);
+    BaseResp search(String index) throws IOException;
 }
