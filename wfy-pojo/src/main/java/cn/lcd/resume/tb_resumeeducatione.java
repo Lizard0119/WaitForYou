38a37093aee7eb626f104fb36cbcc704804cd9ae
小贾 <1152299260@qq.com>
2020-09-09 18:03:25 +0800

    贾    简历pojo的部分的建

---
+++ wfy-pojo/src/main/java/cn/lcd/resume/tb_resumeeducatione.java
@@ -0,0 +1,26 @@
+package cn.lcd.resume;
+
+import com.mysql.fabric.xmlrpc.base.Data;
+
+//教育经历
+public class tb_resumeeducatione {
+
+    //教育经历id
+    private int resumeeducatione_id;
+
+    //学校
+    private String resumeeducatione_school;
+
+    //学历
+    private String resumeeducatione_background;
+
+    //专业
+    private String resumeeducatione_major;
+
+    //时间段
+    private Data resumeeducatione_time;
+
+    //在校经历
+    private String resumeeducatione_experience;
+
+}
---
+++ wfy-pojo/src/main/java/cn/lcd/resume/tb_user_resumeeducatione.java
@@ -0,0 +1,11 @@
+package cn.lcd.resume;
+
+//用户与项目的教育经验
+public class tb_user_resumeeducatione {
+
+//    用户的id
+    private int user_id;
+//    项目的教育经验表id
+    private int resumeeducatione_id;
+
+}
