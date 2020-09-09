be14d65b55ce5ccd98e5de547181e27c0a282ac9
小贾 <1152299260@qq.com>
2020-09-09 16:13:10 +0800

    贾    简历创建

---
+++ wfy-resume/src/main/java/cn/lcd/dao/ResumeDao.java
@@ -0,0 +1,11 @@
+package cn.lcd.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface ResumeDao {
+   // List<> findResuemByUserName(@Param("userName")String userNmame);
+}
